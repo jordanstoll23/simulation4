5e1a6f0a4a8a874fd9dc870ea026a228054c1b3a
jordanstoll23 <jdogstoll@yahoo.com>
2017-10-24 11:58:09 -0700

    nothing changed

--- trivia/triviaCtrl.js
+++ trivia/triviaCtrl.js
@@ -3,5 +3,7 @@ angular.module("triviaTrends").controller('triviaCtrl', function( $scope, trivia
   triviaSrvc.getQuestions().then(function( response){
     $scope.questionData = response.data;
     console.log(response)
-});
+
+    
+  });
 })
