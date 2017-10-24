0aeaf64fd189023a0c81ffac2fa99e7ec2afadd5
Haley Boyle <haleyjboyle@gmail.com>
2017-10-24 13:35:28 -0700

    css stuff

--- trivia/triviaCtrl.js
+++ trivia/triviaCtrl.js
@@ -1,7 +1,37 @@
 angular.module("triviaTrends").controller('triviaCtrl', function( $scope, triviaSrvc){
   
+$scope.q = {
+  "difficulty": 2,
+  "question": "which animal is the hyena more closely related to?",
+  "animal": "hyena",
+  "correct_answer": 3,
+  "options": {
+      "1": "wolf",
+      "2": "african wilddog",
+      "3": "lion",
+      "4": "zebra"
+  }
+}
+
   triviaSrvc.getQuestions().then(function( response){
     $scope.questionData = response.data;
     console.log(response)
 });
+$scope.difficulty = function(difficulty){
+  console.log( difficulty);
+  triviaSrvc.getDifficulty(difficulty).then(function(response){
+    console.log(response);
+    $scope.difficultData = response.data;
+  })
+};
+
+this.addQuestion = function(add) {
+  return $http({
+      method: 'POST',
+      url: 'https://practiceapi.devmountain.com//api/trivia/questions',
+      data: [add],
+  }).then(function(response) {
+      return response.data;
+  })
+}
 })
--- trivia/triviaSrvc.js
+++ trivia/triviaSrvc.js
@@ -3,5 +3,18 @@ angular.module('triviaTrends').service('triviaSrvc', function($http) {
     return $http.get(
        'https://practiceapi.devmountain.com/api/trivia/questions'
     )
+},
+// this.nextPage = function(page) {
+//   return $http({
+//     method: 'GET',
+//     url: `https://practiceapi.devmountain.com/api/trivia/questions?page=${page}`
+//   }).then(function(response) {
+//     return response.data;
+//   })
+// }
+this.getDifficulty = function(difficulty){
+  return $http.get(
+    `https://practiceapi.devmountain.com/api/trivia/questions/difficulty/${difficulty}`
+ )
 }
 });
